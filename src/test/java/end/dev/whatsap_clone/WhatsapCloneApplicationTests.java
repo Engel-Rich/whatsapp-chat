055a1625ec7e65f576d1636e2ee95198d8c025bd
Engelbert Richelieu Mbarga Tsinda <engelbertrichelieutsinda2gmail.com>
2024-11-03 20:36:31 +0100

    Initialisation des datas

---
+++ src/test/java/end/dev/whatsap_clone/WhatsapCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package end.dev.whatsap_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsapCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
